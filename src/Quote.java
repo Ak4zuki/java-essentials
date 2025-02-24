8ac5690f3b3a34b43786d61bfd4d718408b8001d
Stefan Venter <venterstefan492@gmail.com>
2025-02-24 23:10:38 +0200

    updated dependencies

--- src/Quote.java
+++ src/Quote.java
@@ -8,6 +8,8 @@ public class Quote {
     public static void main(String[] args) {
         try{
             String apiUrl = "";
+        }catch (Exception e){
+            e.printStackTrace();
         }
     }
 }
