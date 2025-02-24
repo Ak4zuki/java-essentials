7b60a3c51577d59b1dae8287e293bb632cd43de1
Stefan Venter <venterstefan492@gmail.com>
2025-02-24 17:53:50 +0200

    main commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import service.UserService;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+
+        boolean isStudent = false;
+        final double basePrice = 119.9;
+
+        System.out.println("Welcome to Flick Lo. \nThe base price for a ticket is R" + basePrice + ".");
+
+        System.out.print("\nWhat is your name?: ");
+        String name = scanner.nextLine();
+
+        System.out.print("How old are you?: ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+
+
+        if (UserService.checkAge(age)) {
+            System.out.print("Are you a Student? (yes/no): ");
+            String response = scanner.nextLine().trim().toLowerCase();
+            isStudent = response.equals("yes");
         }
+
+        double finalPrice = UserService.calculatePrice(age, basePrice, isStudent);
+
+        System.out.printf("\nHello %s\n", name);
+        if (finalPrice == 0) {
+            System.out.println("You qualify for a free ticket!");
+        } else {
+            System.out.printf("Your final ticket price is: R%.2f\n", finalPrice);
+        }
+
+        scanner.close();
     }
 }
