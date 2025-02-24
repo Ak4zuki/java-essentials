2c31a1e22c4b6a2e0612537fda63eeb3b9a1860b
Stefan Venter <venterstefan492@gmail.com>
2025-02-24 23:08:54 +0200

    updated dependencies

---
+++ src/Quote.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.json.JSONObject;
+
+public class Quote {
+    public static void main(String[] args) {
+        try{
+            String apiUrl = "";
+        }
+    }
+}
