30ca85d3aed049865c71abd3d2b092396a9a99f1
Stefan Venter <venterstefan492@gmail.com>
2025-02-25 13:23:41 +0200

    created email slicer programme

--- src/Substrings.java
+++ src/Substrings.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Substrings {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        String email;
+        String username;
+        String domain;
+
+        System.out.print("Enter your email: ");
+
+        email = scanner.nextLine().trim().toLowerCase();
+
+        if(email.contains("@")){
+            username = email.substring(0, email.indexOf("@"));
+            domain = email.substring(email.indexOf("@")+1);
+
+            System.out.println(username);
+            System.out.println(domain);
+        }
+        else{
+            System.out.println("\nEmail must contain @ symbol");
+        }
+
+        scanner.close();
     }
 }
