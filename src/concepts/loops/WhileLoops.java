6e50ed2f7153b984c706bf2077f4c0651e1daebf
Stefan Venter <venterstefan492@gmail.com>
2025-02-25 23:35:18 +0200

    Ez Top diff

--- src/concepts/loops/WhileLoops.java
+++ src/concepts/loops/WhileLoops.java
@@ -1,7 +1,20 @@
 package concepts.loops;
+import java.util.Scanner;
 
 public class WhileLoops {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        String name = "";
+
+        do{
+            System.out.print("Enter your name: ");
+            name = scanner.nextLine();
+        }while (name.isEmpty());
+
+        System.out.println("Hello " + name);
+
+        scanner.close();
     }
 }
