e459b0fe862703d9c573a0226bccc952918bf32e
Stefan Venter <venterstefan492@gmail.com>
2025-02-26 17:15:58 +0200

    Created Methods (im cooked)

--- src/concepts/Methods.java
+++ src/concepts/Methods.java
@@ -3,5 +3,18 @@ package concepts;
 public class Methods {
     public static void main(String[] args) {
 
+        int age = 12;
+
+        if (ageCheck(age)){
+            System.out.println("You may sign up");
+        }else {
+            System.out.println("You must be 18+ to sign up");
+        }
+    }
+    static double square(double number) {return number * number;}
+    static double cube(double number) {return number * number * number;}
+    static String getFullName(String first, String last) {return first + " " +last; }
+    static boolean ageCheck(int age){
+        return age >= 18;
     }
 }
