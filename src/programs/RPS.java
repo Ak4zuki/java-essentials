1202a5c1f068080d4f01d7aa0f3fe22392f9bb70
Stefan Venter <venterstefan492@gmail.com>
2025-02-27 17:36:56 +0200

    Rock Paper Scissor game

---
+++ src/programs/RPS.java
@@ -0,0 +1,17 @@
+package programs;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Created class for Rock, Paper, Scissor game.
+ */
+
+public class RPS {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        scanner.close();
+    }
+}
