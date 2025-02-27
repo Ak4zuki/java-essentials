4eaae8fdc19b4101c5264669383eb26a958abbd7
Stefan Venter <venterstefan492@gmail.com>
2025-02-27 15:13:36 +0200

    Ez  claps

--- src/programs/DiceRoller.java
+++ src/programs/DiceRoller.java
@@ -9,17 +9,83 @@ public class DiceRoller {
         Random random = new Random();
 
         int diceNum;
-        int total;
+        int total = 0;
 
-        System.out.print("Enter # of Dice");
+        System.out.print("Enter # of Dice: ");
         diceNum = scanner.nextInt();
 
         if(diceNum > 0){
             for(int i = 0; i < diceNum; i++){
                 int roll = random.nextInt(7);
+                printDie(roll);
                 System.out.println("You rolled " + roll);
+                total += roll;
             }
+            System.out.println("Total: " + total);
         }
 
+        scanner.close();
+    }
+    static void printDie(int roll){
+
+        String dice1 = " -------\n"
+                     + "|       |\n"
+                     + "|   ●   |\n"
+                     + "|       |\n"
+                     + " -------\n";
+
+        String dice2 = " -------\n"
+                     + "|   ●   |\n"
+                     + "|       |\n"
+                     + "|   ●   |\n"
+                     + " -------\n";
+
+        String dice3 = " -------\n"
+                     + "|   ●   |\n"
+                     + "|   ●   |\n"
+                     + "|   ●   |\n"
+                     + " -------\n";
+
+        String dice4 = " -------\n"
+                     + "| ●   ● |\n"
+                     + "|       |\n"
+                     + "| ●   ● |\n"
+                     + " -------\n";
+
+        String dice5 = " -------\n"
+                     + "| ●   ● |\n"
+                     + "|   ●   |\n"
+                     + "| ●   ● |\n"
+                     + " -------\n";
+
+        String dice6 = " -------\n"
+                     + "| ●   ● |\n"
+                     + "| ●   ● |\n"
+                     + "| ●   ● |\n"
+                     + " -------\n";
+
+        switch(roll){
+            case 1 :
+                System.out.print(dice1);
+                break;
+            case 2 :
+                System.out.print(dice2);
+                break;
+            case 3 :
+                System.out.print(dice3);
+                break;
+            case 4 :
+                System.out.print(dice4);
+                break;
+            case 5 :
+                System.out.print(dice5);
+                break;
+            case 6 :
+                System.out.print(dice6);
+                break;
+            default:
+                System.out.print("INVALID");
+                break;
+        }
     }
 }
