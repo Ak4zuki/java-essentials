653147792fd04137b3d7779c7c732dbb453b1d2d
Stefan Venter <venterstefan492@gmail.com>
2025-02-27 17:50:11 +0200

    1st try. we windows

--- src/programs/RPS.java
+++ src/programs/RPS.java
@@ -12,6 +12,23 @@ public class RPS {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
+        String[] choices = {"rock", "paper", "scissor"};
+        String playerChoice;
+        String computerChoice;
+        String playAgain = "yes";
+
+        System.out.print("Choose wise tarnished.\nRock, Paper or Scissor?: ");
+        playerChoice = scanner.nextLine().toLowerCase();
+
+        if(!playerChoice.equals("rock") &&
+                !playerChoice.equals("paper") &&
+                !playerChoice.equals("scissors")){
+            System.out.println("No Strength Builds Allowed, Tarnished");
+        }
+
+        computerChoice = choices[random.nextInt(3)];
+        System.out.println("Yes.\nBut will it beat " + computerChoice + "?");
+
         scanner.close();
     }
 }
