5fb0623f9b3d8a3d13d5f4f1171075a04e5b1c48
Stefan Venter <venterstefan492@gmail.com>
2025-02-28 19:15:00 +0200

    User class for overloaded Constructors

--- src/concepts/oop/examples/User.java
+++ src/concepts/oop/examples/User.java
@@ -1,4 +1,32 @@
 package concepts.oop.examples;
 
 public class User {
+
+    public String username;
+    public String email;
+    public int age;
+
+    public User(){
+        this.username = "Guest";
+        this.email = "Not Provided";
+        this.age = 0;
+    }
+
+    public User(String username) {
+        this.username = username;
+        this.email = "Not provided";
+        this.age = 23;
+    }
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+        this.age = 22;
+    }
+
+    public User(String username, String email, int age) {
+        this.username = username;
+        this.email = email;
+        this.age = age;
+    }
 }
