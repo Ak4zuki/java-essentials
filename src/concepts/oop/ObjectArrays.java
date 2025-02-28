2e467448fa525645e207d6bf4b6bc8b1aeb7a8c0
Stefan Venter <venterstefan492@gmail.com>
2025-02-28 19:46:51 +0200

    Added Constructor

---
+++ src/concepts/oop/ObjectArrays.java
@@ -0,0 +1,15 @@
+package concepts.oop;
+
+import concepts.oop.examples.Car;
+
+public class ObjectArrays {
+    public static void main(String[] args) {
+
+        Car[] cars = {new Car("MK4 Supra", "White"), new Car("R34 Skyline", "Blue"), new Car("RX 7", "Orange")};
+
+        for (Car car : cars) {
+            car.drive();
+        }
+
+    }
+}
