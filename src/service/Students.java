f6fc883b14ebfbc33569cc977c9bf50ff30ce5c1
Stefan Venter <venterstefan492@gmail.com>
2025-02-28 17:46:12 +0200

    Service class for constructors

---
+++ src/service/Students.java
@@ -0,0 +1,13 @@
+package service;
+
+public class Students {
+
+    public String name;
+    public int age;
+    public double gpa;
+    public boolean isEnrolled;
+
+    public Student(){
+
+    }
+}
