0b55e4a5694a634c960ae47a1b42a6fcf1184cec
Stefan Venter <venterstefan492@gmail.com>
2025-02-28 17:39:00 +0200

    Main car class for OOP

---
+++ src/concepts/oop/CarMain.java
@@ -0,0 +1,16 @@
+package concepts.oop;
+
+import service.Car;
+
+public class CarMain {
+    public static void main(String[] args) {
+
+        Car car = new Car();
+
+        System.out.println(car.make);
+        System.out.println(car.model);
+        System.out.println(car.year);
+        System.out.println("R" + car.price);
+        car.stop();
+    }
+}
